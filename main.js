0b80786810885c7414f508a216613634604d8348
Ryan <rolearyprogramming@gmail.com>
2024-05-23 15:38:17 -0600

    Favorite messages shown, stored in local storage, next step is making separate page, deleting

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-
+var favMessageArray = [];
+var favMessageButton = document.querySelector('.favorite-button');
 var messageButton = document.querySelector('.message-button');
-var outputLocation = document.querySelector('.output-box')
+var outputLocation = document.querySelector('.output-box');
+var showButton = document.querySelector('.show-button');
+
+var messageOutput;
+var storedArray;
+favMessageButton.addEventListener('click', favThisMessage);
+
 messageButton.addEventListener('click', messageDisplay);
+showButton.addEventListener('click', showFavs)
 
 
 function getRandomIndex(array) {
@@ -9,17 +17,71 @@ function getRandomIndex(array) {
 }
 
 function messageDisplay() {
-    outputLocation.innerHTML = null;
+    // outputLocation.innerHTML = null;
+    if (!document.querySelector('input[name="radio-message"]:checked')) {
+        outputLocation.innerText = "Please select the kind of message you'd like to read!";
+        return 'invalid';
+    }
 
     var typeDisplayed = document.querySelector('input[name="radio-message"]:checked').value;
-    if (typeDisplayed === 'affirmations') {
-        affirmationOutput = `${affirmations[getRandomIndex(affirmations)]}`;
-        outputLocation.innerHTML = affirmationOutput;
+    if (!typeDisplayed) {
+        outputLocation.innerText = "Please select the kind of message you'd like to read!";
+    }
+    else if (typeDisplayed === 'affirmations') {
+        messageOutput = `${affirmations[getRandomIndex(affirmations)]}`;
+        outputLocation.innerText = messageOutput;
     }
     else {
 
-        var mantraOutput = `${mantras[getRandomIndex(mantras)]}`;
-        outputLocation.innerHTML = mantraOutput;
+        messageOutput = `${mantras[getRandomIndex(mantras)]}`;
+        outputLocation.innerText = messageOutput;
     }
+}
 
+function favThisMessage() {
+    if (!messageOutput) {
+        console.log("messageOutput isn't defined");
+    }
+    else {
+
+        favMessageArray.push(messageOutput);
+        console.log(favMessageArray);
+        if (typeof (Storage)) {
+            if (sessionStorage.favArray) {
+                storedArray = JSON.parse(sessionStorage.getItem('favArray'));
+                storedArray.push(messageOutput);
+                sessionStorage.setItem('favArray', JSON.stringify(storedArray));
+            } else {
+                storedArray = [];
+                storedArray.push(messageOutput);
+                sessionStorage.setItem('favArray', JSON.stringify(storedArray));
+            }
+            console.log(sessionStorage.favArray);
+        }
+    }
+}
+
+function showFavs() {
+    if (typeof (Storage)) {
+        if (sessionStorage.favArray) {
+            storedArray = JSON.parse(sessionStorage.getItem('favArray'));
+            outputLocation.innerHTML = null;
+            for (i = 0; i < storedArray.length; i++) {
+                outputLocation.innerHTML += `${storedArray[i]} <br>`;
+            }
+        }
+    }
+}
+
+
+function sessionStorageTest() {
+    if (typeof (Storage)) {
+        if (sessionStorage.countClick) {
+            sessionStorage.countClick += 1;
+
+        } else {
+            sessionStorage.countClick = 1;
+        }
+        console.log(sessionStorage.countClick.length);
+    }
 }
