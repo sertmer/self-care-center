2c8d83f9a3f0a23a8082659828387dcfbf946c7c
Ryan <rolearyprogramming@gmail.com>
2024-05-24 11:42:52 -0600

    Made new page, added deletion

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ var messageButton = document.querySelector('.message-button');
 var outputLocation = document.querySelector('.output-box');
 var showButton = document.querySelector('.show-button');
 var homeButton = document.querySelector('.home-button');
+var homePage = document.querySelector('.main-page-view');
+var favPage = document.querySelector('.fav-page-view');
+var favOutput = document.querySelector('.fav-show-area');
+var unfavoriteEventArray = [];
 var messageOutput;
 var storedArray;
+
+
 favMessageButton.addEventListener('click', favThisMessage);
 
 messageButton.addEventListener('click', messageDisplay);
@@ -67,17 +73,48 @@ function showFavs() {
     if (typeof (Storage)) {
         if (sessionStorage.favArray) {
             storedArray = JSON.parse(sessionStorage.getItem('favArray'));
-            outputLocation.innerHTML = null;
+            favOutput.innerHTML = null;
             for (i = 0; i < storedArray.length; i++) {
-                outputLocation.innerHTML += `${storedArray[i]} <br>`;
+                favOutput.innerHTML += `${storedArray[i]} <br> <button class="button un-favorite-button" id='unfavorite-${i}'> Unfavorite </button>  <br> <br>`;
+            }
+            for (i = 0; i < storedArray.length; i++) {
+                unfavoriteEventArray[i] = document.querySelector(`#unfavorite-${i}`);
+                unfavoriteEventArray[i].indexValue = i;
+                unfavoriteEventArray[i].addEventListener('click', unFavorite);
+
             }
         }
     }
+
+    showButton.classList.add('hidden');
+    homePage.classList.add('hidden');
+    favPage.classList.remove('hidden');
+    homeButton.classList.remove('hidden');
+
 }
 
-function returnHome()
-{
-return "hey";
+function returnHome() {
+
+    showButton.classList.remove('hidden');
+    homePage.classList.remove('hidden');
+    favPage.classList.add('hidden');
+    homeButton.classList.add('hidden');
+
+    return "hey";
+}
+
+function unFavorite() {
+    var indexToUse = this.indexValue;
+    if (typeof (Storage)) {
+        if (sessionStorage.favArray) {
+            storedArray = JSON.parse(sessionStorage.getItem('favArray'));
+            console.log(indexToUse);
+            storedArray.splice(indexToUse, 1);
+            sessionStorage.setItem('favArray', JSON.stringify(storedArray));
+            showFavs();
+        }
+    }
+    console.log(this.id)
 }
 
 function sessionStorageTest() {
