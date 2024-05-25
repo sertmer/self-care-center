be237a90d52ca377628552b27bae86d1f3383942
Terra Manning <terradmanning@gmail.com>
2024-05-25 10:36:57 -0600

    start pseudocoding for adding js

---
+++ main.js
@@ -0,0 +1,33 @@
+//Iteration 0- To do: separate the radio buttons.
+//Iteration 'Choose your own adventure'- Add a delete feature
+//Other- Complete the read-me file, deploy
+
+//Iteration 1
+
+//Restate the goal: 
+//When a user selects a message option and then clicks the "receive Message" button, the user sees a random message from the list of possible messages for that category.
+//When the message appears, the meditation icon dissapears from the message area.
+
+//Consider the data: 
+//Ask clarifying questions:
+//How many functions do I need to build?
+//Will I need to add anything to HTML in order to make my code work?
+
+//Psuedocode:
+//Research what you don't know:
+//Steps:
+//1. add variables for affirmation sayings and meditation sayings
+//2. add querySelector for affirmation and mantra variables
+//3. 
+
+//Start coding by rerefrencing the psuedocode you've written:
+
+//Refactor/clean up:
+
+var affirmations = [
+
+]
+
+var mantras = [
+    
+]
